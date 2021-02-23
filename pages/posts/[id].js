07752a80ea35e28effdc62fadbc69dd6a1dd427a
Rochak Gupta <rochak84@gmail.com>
2021-02-23 13:14:53 -0800

    Finish tutorial

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css'
+
+const Post = ({ postData }) => (
+    <Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>
+)
+
+export const getStaticPaths = () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default Post;
